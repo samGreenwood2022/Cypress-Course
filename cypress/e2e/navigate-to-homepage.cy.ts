5ed7e9c67810ca36c8cfa246602131e3fcece98f
Sam Greenwood <sam_greenwood26@hotmail.com>
2026-09-21 15:17:32 +0100

    Update homepage navigation test to target Dyson Manufacturer and verify URL changes

--- cypress/e2e/navigate-to-homepage.cy.ts
+++ cypress/e2e/navigate-to-homepage.cy.ts
@@ -1,5 +1,15 @@
-describe('Navigate to Homepage', () => {
+describe('Navigate to Dyson Manufacturer Homepage', () => {
   it('should navigate to the homepage successfully', () => {
     cy.visit('/');
+    // Page URL changed.
+    cy.url()
+      .should('eq', 'https://source.thenbs.com/en/gb')
+    cy.get('[data-cy="searchFieldSearch"]').filter(':visible').click( {timeout: 10000});
+    cy.get('[data-cy="searchFieldSearch"]').filter(':visible').type('dyson{enter}', {timeout: 10000} );
+    cy.get('[data-cy="tabCategory"]').contains('Manufacturers').click( {timeout: 10000});
+    cy.get('[title="View Dyson"]').click( {timeout: 10000});
+    cy.url().should('include', '/manufacturer/dyson/nakAxHWxDZprdqkBaCdn4U/overview');
+    
+    
   });
 });
