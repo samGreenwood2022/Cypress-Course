10de60d24d5aa5bbc84f185a984925667c87a1f3
samGreenwood2022 <120378330+samGreenwood2022@users.noreply.github.com>
2026-09-22 11:44:59 +0100

    Add h1 header verification to Dyson Manufacturer homepage test

--- cypress/e2e/navigate-to-homepage.cy.ts
+++ cypress/e2e/navigate-to-homepage.cy.ts
@@ -24,8 +24,39 @@ describe('Navigate to Dyson Manufacturer Homepage', () => {
       .click({ timeout: 10000 });
     // confirm we're on the Dyson page
     cy.url()
-      .should('include', '/manufacturer/dyson/nakAxHWxDZprdqkBaCdn4U/overview'); // confirm we're on the Dyson page
-
-
+      .should('include', '/manufacturer/dyson/nakAxHWxDZprdqkBaCdn4U/overview');
   });
+
+  // assert the h1 header is correct
+  it('the h1 header is correct', () => {
+    // go to the baseUrl homepage
+    cy.visit('/');
+    // check we landed on the right page
+    cy.url()
+      .should('eq', 'https://source.thenbs.com/en/gb')
+    // click the visible search box
+    cy.get('[data-cy="searchFieldSearch"]')
+      .filter(':visible')
+      .click({ timeout: 10000 });
+    // type 'dyson' into the search box and hit enter
+    cy.get('[data-cy="searchFieldSearch"]')
+      .filter(':visible')
+      .type('dyson{enter}', { timeout: 10000 });
+    // switch to the Manufacturers tab
+    cy.get('[data-cy="tabCategory"]')
+      .contains('Manufacturers')
+      .click({ timeout: 10000 });
+    // click the Dyson result tile
+    cy.get('[title="View Dyson"]')
+      .click({ timeout: 10000 });
+    // confirm we're on the Dyson page
+    cy.url()
+      .should('include', '/manufacturer/dyson/nakAxHWxDZprdqkBaCdn4U/overview');
+
+    // check the h1 header
+    cy.get('h1')
+      .should('be.visible')
+      .and('have.text', 'Dyson');   
+  });
+
 });
